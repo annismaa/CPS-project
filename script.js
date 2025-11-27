e635b8628ea0d21bf35ddf0c2b196431ec7a5019
Anna Ismoyilova <anytaisma99@gmail.com>
2025-11-27 13:43:17 +0300

    repair 1st commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function initSwiper() {
+  const swiper = new Swiper(".brands__swiper", {
+    slidesPerView: 1.25,
+    direction: "horizontal",
+    loop: true,
+    spaceBetween: 16,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  return swiper;
+}
+
+if (document.body.clientWidth < 768) {
+  initSwiper();
+}
+
+const brandsList = document.querySelector(".brands__list");
+const buttonToggle = document.querySelector(".button--toggle");
+
+buttonToggle.addEventListener("click", () => {
+  brandsList.classList.toggle("brands__list--hidden");
+
+  const buttonToggleIcon = buttonToggle.querySelector(".brands__button-icon");
+  buttonToggleIcon.classList.toggle("brands__button-icon--rotated");
+
+  const buttonToggleText = buttonToggle.querySelector(".brands__button-label");
+
+  if (brandsList.classList.contains("brands__list--hidden")) {
+    buttonToggleText.textContent = "Показать все";
+  } else {
+    buttonToggleText.textContent = "Скрыть";
+  }
+});
